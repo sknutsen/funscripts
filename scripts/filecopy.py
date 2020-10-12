90c8b24599dabae1a097e1f257276a596f97d46f
Sondre Knutsen <sondreknutsen1@gmail.com>
2020-10-12 17:06:20 +0200

    less duplicate code

--- scripts/filecopy.py
+++ scripts/filecopy.py
@@ -10,25 +10,21 @@ def backup():
     dest_dir1 = "D:/Backups/Wowui backup/Interface"
     dest_dir2 = "D:/Backups/Wowui backup/WTF"
 
-    try:
-        shutil.rmtree(dest_dir1)
-        shutil.rmtree(dest_dir2)
-        shutil.copytree(src_dir1, dest_dir1, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False)
-        shutil.copytree(src_dir2, dest_dir2, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False)
-    except:
-        print('An error has occurred')
-    else:
-        print('Complete without errors')
+    copy(src_dir1, src_dir2, dest_dir1, dest_dir2)
 
 
 def restore():
     # src_dir = os.getcwd()  # get pwd
-    dest_dir1 = 'E:/World of Warcraft/_retail_/Interface'
-    dest_dir2 = 'E:/World of Warcraft/_retail_/WTF'
-
     src_dir1 = "D:/Backups/Wowui backup/Interface"
     src_dir2 = "D:/Backups/Wowui backup/WTF"
 
+    dest_dir1 = 'E:/World of Warcraft/_retail_/Interface'
+    dest_dir2 = 'E:/World of Warcraft/_retail_/WTF'
+
+    copy(src_dir1, src_dir2, dest_dir1, dest_dir2)
+
+
+def copy(src_dir1, src_dir2, dest_dir1, dest_dir2):
     try:
         shutil.rmtree(dest_dir1)
         shutil.rmtree(dest_dir2)
